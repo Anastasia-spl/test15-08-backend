42a850b943ceb84d2844fabeea7b66e772be0958
AnastasiaPolkovnikova <68498874+AnastasiaPolkovnikova@users.noreply.github.com>
2021-08-16 20:10:30 +0800

    Update tableServices.js

--- src/services/tableServices.js
+++ src/services/tableServices.js
@@ -17,20 +17,25 @@ const getTable = async ({ page, limit, }) => {
 const searchItems = async ({ name, quantity, distance, condition, page, limit }) => {
   const typedLimit = limit > 100 ? 100 : limit;
   const skip = page > 1 ? page * limit - 1 : 0;
+
+  let response;
   let paginatedResponse;
 
   if (name) {
     const typedName = name.toLowerCase();
+    response = await Tables.find({ name: typedName });
     paginatedResponse = await Tables.find({ name: typedName }).select({ __v: 0 }).skip(skip).limit(typedLimit);
   }
 
   const typedCondition = condition === 'equal' ? '$eq' :  condition === 'more' ? "$gt" : "$lt";
 
   if (quantity) {
+    response = await Tables.find({ quantity: { [typedCondition]: quantity } });
     paginatedResponse = await Tables.find({ quantity: { [typedCondition]: quantity } }).select({ __v: 0 }).skip(skip).limit(typedLimit)
   }
 
   if (distance) {
+    response = await Tables.find({ distance: { [typedCondition]: distance } });
     paginatedResponse = await Tables.find({ distance: { [typedCondition]: distance } }).select({ __v: 0 }).skip(skip).limit(typedLimit)
   }
 
@@ -38,8 +43,7 @@ const searchItems = async ({ name, quantity, distance, condition, page, limit })
     throw new QueryError('No items found')
   }
   
-  const queriedItems = await Tables.find();
-  const totalItems = queriedItems.length;
+  const totalItems = response.length;
   const totalPages = totalItems < limit ? 1 : Math.ceil(totalItems / limit);
   return { paginatedResponse, totalItems, totalPages };
 };
